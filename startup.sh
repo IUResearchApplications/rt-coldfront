10c3546a44de63435574a1ffce3a6d42f49d7b49
Matthew Kusz <matt52@roadrunner.com>
2025-04-29 09:57:16 -0400

    Add debug mail server

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@ python ./manage.py migrate
 python ./manage.py collectstatic --noinput
 
 if [ "$BUILD_ENV" == "local" ]; then
+    python -u -m smtpd -c DebuggingServer -n localhost:1025
     python ./manage.py runserver_plus 0.0.0.0:8000 --cert-file /opt/coldfront/cert.pem  --key-file /opt/coldfront/key.pem
 else
     gunicorn --workers 3 --bind unix:coldfront.sock -m 007 coldfront.config.wsgi
