96d6dcbe6d83af59312a0ae401d1fb5c61f88ade
Matthew Kusz <matt52@roadrunner.com>
2025-12-10 09:45:22 -0500

    Add docstrings and rename PI eligibility parameters

--- coldfront/plugins/ldap_misc/utils/ldap_user_search.py
+++ coldfront/plugins/ldap_misc/utils/ldap_user_search.py
@@ -2,6 +2,13 @@ from coldfront.plugins.ldap_user_search.utils import LDAPUserSearch
 
 
 def get_user_info(username: str, ldap_search: LDAPUserSearch = None) -> dict:
+    """
+    Runs an LDAP query to find the info specified in the ENV variable LDAP_USER_SEARCH_ATTRIBUTE_MAP. If the user is not
+    found an empty dictionary is returned.
+    
+    :param username: The username to search for
+    :param ldap_search: An instance of the LDAPUserSearch class used to search for the user
+    """
     if ldap_search is None:
         ldap_search = LDAPUserSearch(None, None)
     user_info = ldap_search.search_a_user(username, "username_only")
@@ -11,6 +18,12 @@ def get_user_info(username: str, ldap_search: LDAPUserSearch = None) -> dict:
 
 
 def get_users_info(usernames: list[str]) -> dict:
+    """
+    Runs an LDAP query for each username to find the info specified in the ENV variable LDAP_USER_SEARCH_ATTRIBUTE_MAP.
+    Returns a dictionary with the key as the username and the value as the results found for that user.
+    
+    :param usernames: List of usernames to search for
+    """
     ldap_search = LDAPUserSearch(None, None)
     results = {}
     for username in usernames:
--- coldfront/plugins/ldap_misc/utils/project.py
+++ coldfront/plugins/ldap_misc/utils/project.py
@@ -3,29 +3,40 @@ from django.conf import settings
 from coldfront.plugins.ldap_misc.utils.ldap_user_search import get_user_info, get_users_info
 
 
-def check_if_pi_eligible(username: str, memberships=None) -> bool:
+def check_if_pi_eligible(project_pi_username: str, project_pi_memberships: list = None) -> bool:
+    """
+    Returns if a project PI is still eligible to be a PI.
+
+    :param project_pi_username: The PI's username
+    :param project_pi_memberships: The PI's list of memberships
+    """
     if not settings.LDAP_ENABLE_PROJECT_PI_ELIGIBLE_ADS_GROUPS:
         return True
 
-    if not memberships:
-        memberships = get_user_info(username).get("memberOf")
+    if not project_pi_memberships:
+        project_pi_memberships = get_user_info(project_pi_username).get("memberOf")
 
-    if not memberships:
+    if not project_pi_memberships:
         return False
 
-    for membership in memberships:
+    for membership in project_pi_memberships:
         if membership in settings.LDAP_PROJECT_PI_ELIGIBLE_ADS_GROUPS:
             return True
 
     return False
 
 
-def check_if_pis_eligible(usernames: list[str]) -> dict:
+def check_if_pis_eligible(project_pi_usernames: list[str]) -> dict:
+    """
+    Returns a dictionary of project PIs with their usernames as the key and their eligibility to be a PI as the value.
+
+    :param project_pi_usernames: A list of project PI usernames
+    """
     if not settings.LDAP_ENABLE_PROJECT_PI_ELIGIBLE_ADS_GROUPS:
         return {}
 
     eligible_statuses = {}
-    users_info = get_users_info(usernames)
+    users_info = get_users_info(project_pi_usernames)
     for username, user_info in users_info.items():
         for user_membersip in user_info.get("memberOf", []):
             eligible = user_membersip in settings.LDAP_PROJECT_PI_ELIGIBLE_ADS_GROUPS
@@ -36,8 +47,13 @@ def check_if_pis_eligible(usernames: list[str]) -> dict:
     return eligible_statuses
 
 
-def check_current_pi_eligibilities(project_usernames: list[str]) -> list[str]:
-    users_info = get_users_info(project_usernames)
+def check_current_pi_eligibilities(project_pi_usernames: list[str]) -> list[str]:
+    """
+    Returns a list of project PI usernames that are not eligible to be a PI.
+
+    :param project_pi_usernames: A list of project PI usernames
+    """
+    users_info = get_users_info(project_pi_usernames)
 
     ineligible_pis = []
     for username, user_info in users_info.items():
