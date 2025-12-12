8566973e3014a8c1bce1e4c850259e8e45a6cc46
Matthew Kusz <matt52@roadrunner.com>
2025-12-12 10:25:34 -0500

    Fix error when the ADDITIONAL_USER_SEARCH_CLASSES env variable has not been set

--- coldfront/plugins/ldap_misc/apps.py
+++ coldfront/plugins/ldap_misc/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+from coldfront.core.utils.common import import_from_settings
+
+ADDITIONAL_USER_SEARCH_CLASSES = import_from_settings("ADDITIONAL_USER_SEARCH_CLASSES", [])
 
 
 class LdapMiscConfig(AppConfig):
@@ -8,5 +11,5 @@ class LdapMiscConfig(AppConfig):
     name = "coldfront.plugins.ldap_misc"
 
     def ready(self):
-        if "coldfront.plugins.ldap_user_search.utils.LDAPUserSearch" not in settings.ADDITIONAL_USER_SEARCH_CLASSES:
+        if "coldfront.plugins.ldap_user_search.utils.LDAPUserSearch" not in ADDITIONAL_USER_SEARCH_CLASSES:
             raise ImproperlyConfigured("ldap_misc requires the ldap_user_search plugin, please enable it")
