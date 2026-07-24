bc6f2345cf35cc11cdf7603a4dbe78705a005f13
Matthew Kusz <kuszm646@gmail.com>
2026-07-24 12:18:47 -0700

    Remove debug_toolbar_urls import

--- coldfront/config/urls.py
+++ coldfront/config/urls.py
@@ -8,7 +8,6 @@ ColdFront URL Configuration
 
 import environ
 import split_settings
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.contrib import admin
 from django.core import serializers
