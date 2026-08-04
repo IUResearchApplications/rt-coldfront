45edd833e6fd293530008b50fabd82c77da7216e
Matthew Kusz <kuszm646@gmail.com>
2026-08-04 12:48:17 -0500

    Fix CAS login not working

--- coldfront/core/user/urls.py
+++ coldfront/core/user/urls.py
@@ -14,14 +14,6 @@ EXTRA_APPS = settings.INSTALLED_APPS
 
 
 urlpatterns = [
-    path(
-        "login",
-        LoginView.as_view(
-            template_name="user/login.html", extra_context={"EXTRA_APPS": EXTRA_APPS}, redirect_authenticated_user=True
-        ),
-        name="login",
-    ),
-    path("logout", user_views.HtmxLogoutView.as_view(), name="logout"),
     path("user-profile/", user_views.UserProfile.as_view(), name="user-profile"),
     path("user-profile/<str:viewed_username>", user_views.UserProfile.as_view(), name="user-profile"),
     path("user-projects-managers/", user_views.UserProjectsManagersView.as_view(), name="user-projects-managers"),
@@ -53,5 +45,5 @@ else:
             ),
             name="login",
         ),
-        path("logout", LogoutView.as_view(next_page=reverse_lazy("login")), name="logout"),
+        path("logout", user_views.HtmxLogoutView.as_view(), name="logout"),
     ]
