a99299adc03491f594fd70379134624f4066f6cf
Matthew Kusz <kuszm646@gmail.com>
2026-08-24 09:25:46 -0500

    Implement PI change request management

--- coldfront/plugins/pi_change_request/urls.py
+++ coldfront/plugins/pi_change_request/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from coldfront.plugins.pi_change_request.views import ProjectPiChangeRequestCenterView, ProjectPiChangeRequestView
+from coldfront.plugins.pi_change_request.views import (
+    ProjectPiChangeApprovalView,
+    ProjectPiChangeDenialView,
+    ProjectPiChangeDetailView,
+    ProjectPiChangeRequestCenterView,
+    ProjectPiChangeRequestView,
+)
 
 urlpatterns = [
     path("<int:pk>", ProjectPiChangeRequestView.as_view(), name="pi-change-request"),
+    path("<int:pk>/approval", ProjectPiChangeApprovalView.as_view(), name="pi-change-request-approval"),
+    path("<int:pk>/denial", ProjectPiChangeDenialView.as_view(), name="pi-change-request-denial"),
+    path("<int:pk>/detail", ProjectPiChangeDetailView.as_view(), name="pi-change-request-details"),
     path("center", ProjectPiChangeRequestCenterView.as_view(), name="pi-change-request-center"),
 ]
