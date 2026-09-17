2f5b3a91c07c1304982dcd2182490697636dbae6
Matthew Kusz <kuszm646@gmail.com>
2026-09-17 12:34:32 -0500

    Notify center when PI change request is ready, read settings via django, and load the signals module in AppConfig

--- coldfront/plugins/pi_change_request/apps.py
+++ coldfront/plugins/pi_change_request/apps.py
@@ -1,6 +1,11 @@
+import importlib
+
 from django.apps import AppConfig
 
 
 class PiChangeRequestConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "coldfront.plugins.pi_change_request"
+
+    def ready(self) -> None:
+        importlib.import_module("coldfront.plugins.pi_change_request.signals")
--- coldfront/plugins/pi_change_request/utils.py
+++ coldfront/plugins/pi_change_request/utils.py
@@ -1,14 +1,11 @@
-from coldfront.core.utils.common import import_from_settings
+from django.conf import settings
+
 from coldfront.core.utils.mail import send_email_template
 from coldfront.core.utils.slack import send_message
 
-EMAIL_ENABLED = import_from_settings("EMAIL_ENABLED", False)
-EMAIL_ALERTS_EMAIL_ADDRESS = import_from_settings("EMAIL_ALERTS_EMAIL_ADDRESS", "")
-SLACK_MESSAGING_ENABLED = import_from_settings("SLACK_MESSAGING_ENABLED", False)
-
 
 def send_slack_message(project_obj, url):
-    if not SLACK_MESSAGING_ENABLED:
+    if not settings.SLACK_MESSAGING_ENABLED:
         return
 
     send_message(
@@ -18,12 +15,27 @@ def send_slack_message(project_obj, url):
 
 def send_email(subject, template, template_context, receiver=None):
     """Send a template email to one or more receivers, defaulting to the center alerts address."""
-    if not EMAIL_ENABLED:
+    if not settings.EMAIL_ENABLED:
         return
 
     if receiver is None:
-        receiver = [EMAIL_ALERTS_EMAIL_ADDRESS]
+        receiver = [settings.EMAIL_ALERTS_EMAIL_ADDRESS]
     elif isinstance(receiver, str):
         receiver = [receiver]
 
     send_email_template(subject, template, template_context, receiver)
+
+
+def send_ready_email(pi_change_request, url):
+    """Notify the center that a PI change request has all its approvals and is ready to activate."""
+    template_context = {
+        "project_title": pi_change_request.project.title,
+        "project_id": pi_change_request.project.pk,
+        "url": url,
+        "help_email": settings.EMAIL_TICKET_SYSTEM_ADDRESS,
+    }
+    send_email(
+        "PI Change Request Ready for Activation",
+        "pi_change_request/email/pi_change_request_ready.txt",
+        template_context,
+    )
